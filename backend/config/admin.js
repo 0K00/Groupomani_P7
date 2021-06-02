ad575f8324aa58452e533de9013a061747722fee
0K0 <qdouvillez@gmail.com>
2021-06-02 06:48:38 +0200

    :sparkles: config add

---
+++ backend/config/admin.js
@@ -0,0 +1,38 @@
+const db = require('../models');
+const bcrypt = require('bcrypt');
+
+function setAdmin(req, res) {
+	db.User.findOne({ where: { email: 'admin@mail.com' } || { pseudo: 'admin' } })
+		.then(user => {
+			if (!user) {
+				bcrypt
+					.hash('Moderator', 10)
+					.then(hash => {
+						const admin = db.User.create({
+							pseudo: 'admin',
+							email: 'admin@mail.com',
+							password: hash,
+							admin: true
+						})
+							.then(admin => {
+								console.log({
+									admin,
+									message: `Your admin account has been created ${admin.pseudo}!`
+								});
+							})
+							.catch(error => {
+								res.status(400).json({ error });
+							});
+					})
+					.catch(error => {
+						res.status(500).send({ error });
+					});
+			} else {
+				console.log({ message: 'the admin is already created' });
+			}
+		})
+		.catch(error => {
+			console.log({ error });
+		});
+}
+module.exports = setAdmin();
