5ac2238bf5aa1421c2a8aa4e24891b20cc54c8b3
0K0 <qdouvillez@gmail.com>
2021-06-03 15:59:09 +0200

    :rocket: App.vue add

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <router-view></router-view>
+    <!-- Display view pages here based on route -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data: () => {
+    return {};
+  },
+  computed: {
+    user() {
+      return this.$store.getters.user;
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getUsers");
+    this.$store.dispatch("getUserById", this.user.id);
+  },
+};
+</script>
+<style lang="scss"></style>
